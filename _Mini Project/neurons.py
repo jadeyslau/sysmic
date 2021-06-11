c5768175e2a66a91b5d8598e052a5aecaf69784e
Jade Lau <jadeyslau@gmail.com>
2021-06-11 19:27:55 +0100

    Stats course

---
+++ _Mini Project/neurons.py
@@ -0,0 +1,18 @@
+
+
+
+class Neuron(obj):
+    def __init__():
+
+
+# class HHNeuron(Neuron):
+
+class FNNeuron(Neuron):
+    """FitzHugh-Naguno neuron.
+    The units in this model are different from the HH ones.
+    Sources:
+    https://github.com/ruhugu/brainythings/blob/master/brainythings/neurons.py
+    https://en.wikipedia.org/w/index.php?title=FitzHugh%E2%80%93Nagumo_model&oldid=828788626
+    http://www.scholarpedia.org/article/FitzHugh-Nagumo_model
+    """
+    def __init__():
