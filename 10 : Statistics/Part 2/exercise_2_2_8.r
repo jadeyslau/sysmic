7c0f4dbbdd7bb7e0835ce2b1e1ad49bc454f80b2
Jade Lau <jadeyslau@gmail.com>
2021-07-25 15:30:05 +0100

    Fixed iteration

--- 10 : Statistics/Part 2/exercise_2_2_8.r
+++ 10 : Statistics/Part 2/exercise_2_2_8.r
@@ -1,13 +1,12 @@
 p_vector <- vector(mode="numeric")
 
-for(i in 1000 ){
-  sample[i] <- rnorm(n=20,mean=0,sd=1)
-  print(sample[i])
+for(i in c(1:1000) ){
+  sample <- rnorm(n=20,mean=0,sd=1)
   p_val  <- t.test( sample )$p.value
   p_vector[i] <- p_val
 }
 
-print(p_vector)
+# print(p_vector)
 ninety <- which(p_vector < 0.1)
 ninety_five <- which(p_vector < 0.05)
 ninety_nine <- which(p_vector < 0.01)
