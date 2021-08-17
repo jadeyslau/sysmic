feb03933db21a3325cfd2a525668369fd07fb64c
Jade Lau <jadeyslau@gmail.com>
2021-08-17 20:46:16 +0100

    steady state

--- _Mini Project/neurons.py
+++ _Mini Project/neurons.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.integrate import odeint
+from scipy.optimize import fsolve
+
 import matplotlib.pyplot as plt
 
 class Neuron():
@@ -28,7 +30,7 @@ class FHN_Neuron(Neuron):
         # self.I_ext = I_ext
         print('FHN Neuron __init__() called', I_ext)
 
-    def dx_dt(self, x, t, I):
+    def dx_dt(self, x, I):
         dvdt = x[0] - (x[0]**3/3) - x[1] + I
         dwdt = (x[0] + self.a - self.b*x[1]) / self.tau
         return [dvdt,dwdt]
@@ -36,6 +38,25 @@ class FHN_Neuron(Neuron):
     def solve(self, x0, I, t):
         y = odeint(self.dx_dt,x0,t,args=(I,))
         return y
+
+    def solve_ss(self, I):
+        # eq = fsolve(self.dx_dt, self.x0, I)
+        initial_guess = [-1, -1]
+        eq = fsolve(self.dx_dt, self.x0, I)
+        return eq
+
+    # def diff_eqs(x, I):
+    # V, w = x
+    # return [V - V**3 - w + I, .1*(V - 0.5*w+1)]
+
+    # def get_fixed_point(I):
+    #   initial_guess = [-1, -1]
+    #   fixed_point = opt.fsolve(diff_eqs, initial_guess, I)
+    #   return fixed_point
+#
+#     V_fp, w_fp = get_fixed_point(I=0)
+#     print(V_fp)
+#     print(w_fp)
 
     def plot(self, I, t):
         y = self.solve(self.x0, I, t)
