3a6bf133ccfa83617d280dbe508530d42f6132b9
Jade Lau <jadeyslau@gmail.com>
2021-09-08 21:53:48 +0100

    MBA

--- _Mini Project/neurons.py
+++ _Mini Project/neurons.py
@@ -5,11 +5,25 @@ from scipy.optimize import fsolve
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# t = np.linspace(0, 800, num=3000)
+I_on  = 150
+I_off = 600
+
+A=0.7
+B=0.8
+TAU=12.5
+
 class Neuron(object):
-    def __init__(self, I_ext=0):
+    def __init__(self, I_ext=0 ):
         self.I_ext = I_ext
         self.type = False
-        # print('Neuron __init__() called')
+
+        self.t = np.linspace(0, 800, num=3000)
+
+        # Seaborn graph aesthetics
+        sns.set_style('darkgrid')
+        sns.set_context('paper')
+
 
     def self_identifies(self):
         if not self.type:
@@ -17,38 +31,34 @@ class Neuron(object):
         else:
             return "I'm a %s neuron!" % self.type
 
-    def plot(self, y, t, ax=None, save=[False,None]):
+    def plot(self, y, ax=None, save=[False,None]):
         singleplt = False
-        # print(ax)
+
         if ax is None:
             singleplt = True
-            # ax = plt.gca()
-        # ax.plot(t,y[0])
 
-        sns.set_style('darkgrid')
-
-        # plt.legend(self.legend)
         plt.xlabel('Time (ms)')
-        plt.ylabel('v, w')
+        plt.ylabel(self.label)
         plt.title(y[2])
-        # if y[2]:
-        #     plt.title(y[2])
-        # else:
-        #     plt.title("Applied current: %s mA/cm2" % y[1])
-        # plt.show()
 
         if singleplt:
-            plt.plot(t,y[0])
+            plt.plot(self.t,y[0][:,0:2])
+            # if len(y[0][:,2]) > 0:
+            #     plt.plot(t,y[0][:,2])
+
             plt.legend(self.legend)
+
             if save[0] == True:
                 plt.savefig('Figures/'+save[1]+'.eps', format='eps')
                 print('Figure saved as '+save[1])
             return plt.show()
         else:
-            ax.plot(t,y[0])
+            ax.plot(self.t,y[0][:,0:2])
+
+
             plt.legend(self.legend)
             return ax
-        # return fig
+
 
 class FHN_Neuron(Neuron):
     # Fitzhugh-Nagumo equations:
@@ -60,65 +70,93 @@ class FHN_Neuron(Neuron):
     # a, b, tau parameters of the model
     # 𝑎 = 0.7, 𝑏 = 0.8 and 𝜏 = 12.5.
     #x0=[0.7,-0.5]
-    def __init__(self, I_ext=1, a=0.7, b=0.8, tau=12.5, x0=[0.7,-0.5]):
+    def __init__(self, I_ext=1, a=A, b=B, tau=TAU, x0=[-1.2, -0.6]):
         super().__init__()
         self.a = a
         self.b = b
         self.tau = tau
         self.x0 = x0
         self.legend = ['v','w']
+        self.label  = 'v, w'
 
         self.type = "FitzHugh-Nagumo"
-        # print(self.a, self.b, self.tau)
 
-        # self.I_ext = I_ext
-        # print('FHN Neuron __init__() called')
 
-    def dx_dt(self, x, t, I):
-        if t < 50 or t > 200:
+    def dx_dt(self, x, t, I, fsolv=False):
+        if ((not fsolv) and (t < I_on or t > I_off)):
             I = 0
-
-        # print(t, I)
         dvdt = x[0] - (x[0]**3/3) - x[1] + I
         dwdt = (x[0] + self.a - self.b*x[1]) / self.tau
         return [dvdt,dwdt]
 
-    def solve(self, I, t):
-        y = odeint(self.dx_dt, self.x0, t, args=(I,))
+    def solve(self, I):
+        y = odeint(self.dx_dt, self.x0, self.t, args=(I,))
         title = ("I = %s, a = %s, b = %s, tau = %s" % (I,self.a,self.b,self.tau))
-        # print(y)
+        # print( title, 'x0:', self.x0 )
         return y, I, title
 
-    def solve_ss(self, t, I=0):
-        eq = fsolve(self.dx_dt, self.x0, args=(t,I))
+    def solve_ss(self, I=0):
+        fsolv = True
+        eq = fsolve(self.dx_dt, self.x0, args=(self.t,I,fsolv))
         return "Roots: v = %s, w = %s" % (eq[0], eq[1])
 
 class Rinzel_Neuron(FHN_Neuron):
-    def __init__(self, I_ext=1, e=0.0001, c=-0.775, x0=[0.5,-0.7,-0.5]):
+    def __init__(self, I_ext=1, a=A, b=B, tau=TAU, e=0.0001, c=-0.775, x0=[-1.02,-0.4,0.25]):
         super().__init__()
+
+        # self.t = np.linspace(0, 5000, num=3000)
+
         self.I_ext = I_ext
+        self.a = a
+        self.b = b
+        self.tau = tau
+
         self.e  = e
         self.c  = c
         self.x0 = x0
         self.legend = ['v', 'w', 'z']
-
+        self.label  = ['v, w','z']
+        print(self.a, self.b, self.tau, self.e, self.c)
         self.type = "Rinzel"
 
-        # print('Rinzel Neuron __init__() called')
 
-    def dx_dt(self, x, t, I):
+    def dx_dt(self, x, t, I, fsolv=False):
         # x = [v,w,z]
+        if ((not fsolv) and (t < I_on or t > I_off)):
+            I = 0
         dvdt = x[0] - (x[0]**3/3) - x[1] + x[2] + I
         dwdt = (x[0] + self.a - self.b*x[1]) / self.tau
-        dzdt = self.e* (-x[0] + self.c - x[2])
+        dzdt = self.e * (-x[0] + self.c - x[2])
+
         return [dvdt,dwdt,dzdt]
 
-    def solve(self, I, t):
-        # print(I, t, z)
-        y = odeint(self.dx_dt, self.x0, t, args=(I,))
+    def solve(self, I):
+        y = odeint(self.dx_dt, self.x0, self.t, args=(I,))
         title = "Applied current: %s mA/cm2, e = %s, c = %s" % (I,self.e,self.c)
         return y, I, title
 
-    def solve_ss(self, t, I=0):
-        eq = fsolve(self.dx_dt, self.x0, args=(t,I))
+    def solve_ss(self, I=0):
+        fsolv = True
+        eq = fsolve(self.dx_dt, self.x0, args=(self.t,I,fsolv))
         return "Roots: v = %s, w = %s, z = %s" % (eq[0], eq[1], eq[2])
+
+    def plot(self, y, ax=None, save=[False,None]):
+        singleplt = False
+        if ax is None:
+            singleplt = True
+            fig,ax = plt.subplots(2, sharex=True)
+
+        plt.xlabel('Time (ms)')
+        # plt.ylabel(self.label[0])
+        plt.suptitle(y[2])
+
+
+        ax[0].plot(self.t,y[0][:,0:2]);
+        ax[0].set_ylabel(self.label[0])
+        ax[0].legend(self.legend[0:2])
+
+        ax[1].plot(self.t,y[0][:,2]);
+        ax[1].set_ylabel(self.label[1])
+        ax[1].legend(self.legend[2])
+
+        return ax
